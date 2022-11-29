6fe113a7a3e9a503ee13e4fceb2b60abc38ef2f1
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-29 08:02:56 +0300

    Added changes 29.11.2022

--- practicalWork2.py
+++ practicalWork2.py
@@ -25,3 +25,48 @@ print(d[4] + ' ' + ch)
 # 5. Slicing
 chm = d[1: 3]
 print(chm)
+# 6.
+print(d[1:])
+print(d[:3])
+print(d[:])
+print(d[1:5:2])
+# 7.
+# d[2] = 'o'    !!! Error: 'str' object does not support item assignment !!!
+
+# Working with numbers
+# 1.
+a = 251
+b = 25
+# 2.
+print(a / b)
+print(a // b)
+print(b / a)
+print(a % b)
+print(b ** a)
+# 3.
+# param = 'string' + 123    !!! Error: can only concatenate str (not "int") to str !!!
+param = 'string' + str(555)
+print(param)
+
+# Data Conversion
+# 1.
+param = 'string' + str(555)
+print(param)
+
+# 2.
+n1 = input('Enter the first number:')
+n2 = input('Enter the second number:')
+n3 = float(n1) + float(n2)
+print(n1 + ' plus ' + n2 + ' = ', n3)
+
+# String format
+# 2.
+a = 1 / 3
+print("{:7.3f}".format(a))
+# 3.
+a = 2 / 3
+b = 2 / 9
+print("{:7.3f} {:7.3f}".format(a, b))
+print("{:10.3e} {:10.3e}".format(a, b))
+# 4.
+print(n1 + ' plus ' + n2 + ' = ', '{:10.3f}'.format(n3))
