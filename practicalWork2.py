5bb146a4258333a708d79f2bd3f79690c28a032e
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-29 07:37:00 +0300

    Initial commit

---
+++ practicalWork2.py
@@ -0,0 +1,9 @@
+# Laboratory job #2.
+# Data types
+
+#String
+# 1.
+string1 = 'This is a string.     '
+string2 = '     This is another string.'
+# 2.
+print('Concatenation: ' + string1 + ' ' + string2)
