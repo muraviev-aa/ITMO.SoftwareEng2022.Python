2138ee62d081edd0eea4419c73a623ee5837abcb
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-30 16:43:30 +0300

    Added practicalWork3

---
+++ practicalWork3.py
@@ -0,0 +1,25 @@
+# PracticalWork3.
+
+import math
+import random
+import statistics
+
+nums = [random.randint(0, 100) for i in range(10)]
+print(*nums)
+
+# подсчет суммы чисел списка,
+sumNums = math.fsum(nums)
+
+# подсчет среднего значения,
+mediumNums = statistics.mean(nums)
+
+# подсчет медианы (median)
+medianNums = statistics.median(nums)
+
+# подсчет стандартного отклонения (stdev),
+devNums = statistics.stdev(nums)
+
+print(sumNums)
+print(mediumNums)
+print(medianNums)
+print("{:.2f}".format(devNums))
