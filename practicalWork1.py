ad2b5e485c5c6209557fb802be82561587572b9a
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-30 17:42:49 +0300

    Added practicalWork1

---
+++ practicalWork1.py
@@ -0,0 +1,21 @@
+# PracticalWork1.
+# Проверка работоспособности установки
+# Проверка интерпретатора Python
+# Проверка интегрированной среды разработки
+# Применение команд Python
+
+import time
+delay = 3
+a = int(input())
+print(a**2)
+time.sleep(delay)
+
+
+import math
+#help (math.sqrt)
+#print (dir(math))
+#
+list1 = [82, 8, 23, 97, 92]
+dir(list1)
+help(list1.insert)
+#
